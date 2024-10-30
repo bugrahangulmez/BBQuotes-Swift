7256ff1f8f3e9e64df5ec06f8a59417e9d0ab1cc
bugrahan gulmez <114365074+bugrahangulmez@users.noreply.github.com>
2024-10-31 00:12:05 +0300

    Initial Commit

---
+++ BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by Bugrahan on 31.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
